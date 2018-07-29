28943c5e5b948b348be65723900f04b0157be201
Ricky <Ricky@Hosfelt.io>
2018-07-29 16:04:06 +0100

    Initial commit for quicli template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate quicli;
+
+use quicli::prelude::*;
+
+// Add cool slogan for your app here, e.g.:
+#[derive(Debug, StructOpt)]
+struct Cli {
+    #[structopt(long = "greeting", short = "g", default_value = "world")]
+    greeting: String,
+    #[structopt(flatten)]
+    verbosity: Verbosity,
 }
+
+main!(|args: Cli, log_level: verbosity| {
+    println!("Hello, {}!", args.greeting);
+});
