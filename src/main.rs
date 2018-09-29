ef6da383b2666c1f319c6ae1eeb920aed3e53489
Ricky <Ricky@Hosfelt.io>
2018-09-29 14:54:17 +0100

    Cargo fmt run

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("{{project-name}}")
-                          .version("1.0")
-                          .author("{{authors}}")
-                          .about("Howdy, Rust!")
-                          .arg(Arg::with_name("greeting")
-                               .short("g")
-                               .long("greeting")
-                               .default_value("world")
-                               .help("Sets a greeting for this hello program")
-                               .takes_value(true))
-                          .get_matches();
+        .version("1.0")
+        .author("{{authors}}")
+        .about("Howdy, Rust!")
+        .arg(
+            Arg::with_name("greeting")
+                .short("g")
+                .long("greeting")
+                .default_value("world")
+                .help("Sets a greeting for this hello program")
+                .takes_value(true),
+        ).get_matches();
 
     let greeting = matches.value_of("greeting").unwrap_or_default();
     println!("Hello, {}!", greeting);
