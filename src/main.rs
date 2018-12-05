74e3428d259edd50ab8587196f8e964d1ffe2ac3
Ricky <Ricky@Hosfelt.io>
2018-12-05 13:46:15 +0000

    Updated template file to return result from main

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ use std::error::Error;
 use std::io::Read;
 use std::io::stdin;
 
-fn parse_input(input: String) {
+fn parse_input(input: String) -> Result<(), Box<Error>> {
     //magic for parsing input to a format I need
+
+    Ok(())
 }
 
 //more functions for solving the problem!
 
-fn main() {
+fn main() -> Result<(), Box<Error>> {
     let mut puzzle_input = String::new();
-        stdio::read_to_string(&mut puzzle_input)?;
+        stdin().read_to_string(&mut puzzle_input)?;
 
     println!("Part one: {}", );
     println!("Part two: {}", );
+
+    Ok(())
 }
